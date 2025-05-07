9952e23783e5b17f2db4bd8e7a641149cc4555fc
Nava Nizard <navanizard@Nava-Laptop.>
2025-05-07 16:00:24 -0400

    Added the hpp file with all function declarations and the Board.cpp with constuctors and winning methods defined.

---
+++ Board.cpp
@@ -0,0 +1,261 @@
+/*
+Author: Nava Karine Nizard
+Course: CSCI-135
+Instructor: Professor Tong Yi
+Date: April 20, 2025
+Assignment: Project 3 (Tasks A-B)
+
+This program simulates a two-player Connect4 game from the command line.
+*/
+
+#include "Board.hpp"
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+struct Coord {
+    int bin;
+    int idx;
+};
+
+//Default constructor
+Board::Board(){
+    numBins = 6; //length or columns
+    capacity = 4; //height or rows
+    //push (empty) vertical vectors (bins)
+    for (int numRows = 0; numRows < numBins; numRows++){
+        vector<int> v1;
+        grid.push_back(v1);
+    } 
+    
+}
+
+//Parametized constructor
+Board::Board(int numBins, int capacity){
+    if (numBins < 5){
+        numBins = 5;
+    }
+    if (capacity < 4){
+        capacity = 4;
+    }
+    this->numBins = numBins; //length or columns
+    this->capacity = capacity; //height or rows
+    //push (empty) vertical vectors (bins)
+    for (int numRows = 0; numRows < numBins; numRows++){
+        vector<int> v1;
+        grid.push_back(v1);
+    }
+}
+
+//Make the board line shape
+void print(int numBins){
+    cout << "+";
+    for(int i = 0; i < numBins; i++){
+        cout << "--+";
+    }
+    cout << endl;
+}
+
+//Display the current board
+void Board::display() const {
+    string mapping[] = {
+        "\033[31m\u2b24\033[0m", // 0 = red circle (🔴)
+        "\033[34m\u2b1f\033[0m", // 1 = blue pentagon (🔷)
+        "\033[31m\u25c9\033[0m", // 2 = red double circle (⭕)
+        "\033[34m\u2b54\033[0m"  // 3 = blue empty pentagon (🔺)
+    };
+
+    print(numBins);
+    for (int j = capacity - 1; j >= 0; j--){ //Loop through each horizontal row
+        cout << "|";
+        for (int i = 0; i < numBins; i++){ //Move across the bins
+            //Check if there's a ball in the bin and display it
+            if (j < grid[i].size()){ //if there are elements to display
+                cout << mapping[grid[i][j]] << " |";
+            }
+            //If there're no balls, display a space
+            else{
+                cout << "  |";
+            }
+        }
+        cout << endl;
+        print(numBins);
+    }
+
+    //Print out the labels of the column numbers
+    for (int i = 0; i < numBins; i++){
+        cout << " " << i ;
+    }
+}
+
+int Board::add(int player){
+    //Player ID is either 0 or 1
+    int binIdx;
+    cout << "Enter a bin index in [0, " << numBins << ") that is not full: ";
+    cin >> binIdx;
+    while ((binIdx < 0 || binIdx >= numBins)){
+        cout << "Invalid bin entered. Must be in [0, " << numBins << "). Reenter a bin index: ";
+        cin >> binIdx;
+    }
+    while(grid[binIdx].size() >= capacity){
+        cout << "The bin entered is full. Reenter bin index: ";
+        cin >> binIdx;
+    }
+
+    grid[binIdx].push_back(player);
+    return binIdx;
+}
+
+int Board::winInHorizontal(int bin){
+    int idx = grid[bin].size() - 1; //idx of last element added to the bin
+    int player = grid[bin][idx]; //gets the ID of player who last played
+    vector<Coord> candidates; //saves the bin and idx of each shape sharing player along the anti-diagonal path
+
+    //the first possible winning candidate
+    candidates.push_back({bin, idx});
+
+    //find all right neighbors
+    int currBin = bin + 1;
+    
+    while (currBin < numBins && idx < grid[currBin].size() && grid[currBin][idx] == player){
+        candidates.push_back({currBin, idx});
+        currBin++;
+    }
+
+    //find all left neighbors
+    currBin = bin - 1;
+
+    while (currBin >= 0 && idx < grid[currBin].size() && grid[currBin][idx] == player){
+        candidates.push_back({currBin, idx});
+        currBin--;
+    }
+
+    //check if there are four in a row (horizontally) to win
+    if (candidates.size() >= 4){
+        //change the winning sequence to a diff color to diffrentiate
+        for (int i = 0; i < 4; i++){
+            grid[candidates[i].bin][idx] = player + 2;
+        }
+        return player;
+    }
+
+    return -1; ///If no winner, return -1
+}
+
+int Board::winInVertical(int bin){
+    int idx = grid[bin].size() - 1;
+    int player = grid[bin][idx];
+    vector<Coord> candidates;
+
+    //first possible candidate
+    candidates.push_back({bin, idx});
+
+    //find all lower plays
+    int currIdx = idx - 1;
+
+    while (currIdx >= 0 && grid[bin][currIdx] == player){
+        candidates.push_back({bin, currIdx});
+        currIdx--;
+    }
+
+    //check if there are four in a row (vertically)
+    if (candidates.size() >= 4){
+        //change the winning sequence to a diff color to diffrentiate
+        for (int i = 0; i < 4; i++){
+            grid[bin][candidates[i].idx] = player + 2;
+        }
+        return player;
+    }
+
+    return -1; //if no winner is found, return -1
+}
+int Board::winInDiagonal(int bin){
+    int idx = grid[bin].size() - 1; //idx of last element added to the bin
+    int player = grid[bin][idx]; //gets the ID of player who last played
+    vector<Coord> candidates; //saves the bin and idx of each shape sharing player along the anti-diagonal path
+
+    //the first possible winning candidate
+    candidates.push_back({bin, idx});
+
+    //ANTI-DIAGONAL:
+    //find top left neighbors
+    int currBin = bin - 1;
+    int currIdx = idx + 1;
+
+    //verifies that the pairs are consecutive and the indeces are valid
+    while(currBin >= 0 && currIdx < grid[currBin].size() && grid[currBin][currIdx] == player){
+        candidates.push_back({currBin, currIdx}); //add it if its a match. save in case it wins
+        //Update variables
+        currBin--;
+        currIdx++;
+    }
+
+    //find bottom right neighbors
+    currBin = bin + 1;
+    currIdx = idx - 1;
+
+    //currBin < numBins or grid.size() (amount of elements in grid//vectors)
+    while(currBin < numBins && currIdx >= 0 && grid[currBin][currIdx] == player){
+        candidates.push_back({currBin, currIdx});
+        currBin++;
+        currIdx--;
+    }
+
+    //DIAGONAL:
+    //find top right neighbor
+    currBin = bin + 1;
+    currIdx = idx + 1;
+
+    while (currBin < numBins && currIdx < grid[currBin].size() && grid[currBin][currIdx] == player){
+        candidates.push_back({currBin, currIdx});
+        currBin++;
+        currIdx++;
+    }
+
+    //find bottom left neighbor
+    currBin = bin - 1;
+    currIdx = idx - 1;
+
+    while (currBin >= 0 && currIdx >= 0 && grid[currBin][currIdx] == player){
+        candidates.push_back({currBin, currIdx});
+        currBin--;
+        currIdx--;
+    }
+
+    //check if there are 4 in a row (diagonally or antidiagonally)
+    if (candidates.size() >= 4){
+        //change the winning sequence to a diff color to diffrentiate
+        for (int i = 0; i < 4; i++){
+            grid[candidates[i].bin][candidates[i].idx] = player + 2;
+        }
+        return player;
+    }
+
+    return -1;
+}
+
+
+int Board::win(int bin){
+    int winningPlayer = grid[bin][grid[bin].size() - 1]; //last person who played
+
+    if (winInHorizontal(bin)){
+        return winningPlayer;
+    }
+
+    if (winInVertical(bin)){
+        return winningPlayer;
+    }
+
+    if (winInDiagonal(bin)){
+        return winningPlayer;
+    }
+
+    cout << "No winner.";
+
+    return -1;
+}
+void Board::play(){
+    return;
+}
---
+++ Board.hpp
@@ -0,0 +1,33 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class Board{
+    public: 
+        Board(); //6 bins, each holds at most 4 balls
+        Board(int numBins, int capacity); //numBins, each holds at most capacity
+        void display() const;
+        int add(int player); //given a player, return which bin the player is added to
+        int winInHorizontal(int bin);
+        int winInVertical(int bin);
+        int winInDiagonal(int bin);
+        int win(int bin); //column must be the most recent ball in that bin
+        void play();
+        //Make public for the main file to be able to access it
+        int numBins; //number of bins / LENGTH
+        int capacity; //maximum number of shapes held in each bin / HEIGHT
+        vector<vector<int>> grid;
+    
+    //COMMENT OUT PRIVATE FOR GRADESCOPE
+    // private: 
+        // int numBins; //number of bins / LENGTH
+        // int capacity; //maximum number of shapes held in each bin / HEIGHT
+        // vector<vector<int>> grid;
+};
+
+void print(int numBins);
+#endif
